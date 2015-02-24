34e95bd5193ff567ac38acff97a83dffa3877f15
Sean <seaxbutler@gmail.com>
2015-02-23 20:44:34 -0800

    Added about us page.

--- src/main/java/csusb/campussafety/MenuPage.java
+++ src/main/java/csusb/campussafety/MenuPage.java
@@ -1,16 +1,13 @@
 package csusb.campussafety;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.RelativeLayout;
 
 
-public class MenuPage extends ActionBarActivity implements View.OnClickListener{
+public class MenuPage extends Activity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,28 +26,6 @@ public class MenuPage extends ActionBarActivity implements View.OnClickListener{
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_menu_page, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
     public void onClick(View v) {
         switch(v.getId()){
             case R.id.services:
@@ -60,8 +35,7 @@ public class MenuPage extends ActionBarActivity implements View.OnClickListener{
                 startActivity(new Intent(this, Resources.class));
                 break;
             case R.id.aboutus:
-                //startActivity(new Intent(this AboutUs.class));
-                Log.i("Missing Class", "Not done!");
+                startActivity(new Intent(this, AboutUs.class));
                 break;
             case R.id.tips:
                 startActivity(new Intent(this, AnonymousTips.class));
